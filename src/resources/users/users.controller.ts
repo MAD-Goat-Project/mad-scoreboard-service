6c9d7b74747464a0490ba69ce5db12c2a9ee371a
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-06-20 13:50:31 +0100

    Add roles to admin and app-user

--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -15,7 +15,11 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthenticatedUser } from 'nest-keycloak-connect';
+import {
+  AuthenticatedUser,
+  RoleMatchingMode,
+  Roles,
+} from 'nest-keycloak-connect';
 
 @Controller('users')
 export class UsersController {
@@ -44,16 +48,19 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @Roles({ roles: ['realm:app-user'], mode: RoleMatchingMode.ALL })
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Roles({ roles: ['realm:app-user'], mode: RoleMatchingMode.ALL })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @Roles({ roles: ['realm:app-user'], mode: RoleMatchingMode.ALL })
   @Get('/client/:clientId')
   async findByClientID(@Param('clientId') clientId: string) {
     const client = await this.usersService.findByClientID(clientId);
@@ -63,16 +70,19 @@ export class UsersController {
     return client;
   }
 
+  @Roles({ roles: ['realm:app-admin'], mode: RoleMatchingMode.ALL })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @Roles({ roles: ['realm:app-admin'], mode: RoleMatchingMode.ALL })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
 
+  @Roles({ roles: ['realm:app-user'], mode: RoleMatchingMode.ALL })
   @Get('/generate/:factor')
   generate(@Param('factor') factor: string) {
     const name = this.usersService.generate(factor);
