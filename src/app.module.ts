f216aa87cf277b67562a7f41551fd03b138122d2
Luís Ventuzelos <luisventuzelos@gmail.com>
2023-07-06 23:10:57 +0100

    Activated keycloak validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './resources/users/users.module';
 import { ConfigModule } from '@nestjs/config';
-import { KeycloakConnectModule } from 'nest-keycloak-connect';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
 import { KeycloakConfigService } from './config/keycloak-config.service';
 import { AppConfigModule } from './config/app-config.module';
 import { MongoProviderModule } from './providers/mongo/mongo.provider.module';
 import { PointsModule } from './resources/points/points.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -23,18 +29,18 @@ import { PointsModule } from './resources/points/points.module';
   controllers: [AppController],
   providers: [
     AppService,
-    /*    {
-          provide: APP_GUARD,
-          useClass: AuthGuard,
-        },
-        {
-          provide: APP_GUARD,
-          useClass: ResourceGuard,
-        },
-        {
-          provide: APP_GUARD,
-          useClass: RoleGuard,
-        },*/
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
   ],
 })
 export class AppModule {}
